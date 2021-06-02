7fb944f2f44ec083eb2a049a75079bb59b3e97ae
samenc99 <mateus.enc@gmail.com>
2021-06-02 08:03:48 -0300

    campos de senha

--- src/pages/Sign/SignUp.jsx
+++ src/pages/Sign/SignUp.jsx
@@ -1,20 +1,22 @@
 import All from "../../styleAll/styledAll";
-import {Logo, MyAllContent, MyForm, MyInput, Text} from "./styled";
+import {DivInput, Logo, MyAllContent, MyForm, MyIconButton, MyInput, Text} from "./styled";
 import logoRappi from '../../assets/logo-rappi.svg'
 import {useEffect, useRef, useState} from "react";
 import useForm from "../../hooks/useForm";
 import {StylesProvider} from "@material-ui/core/styles";
 import cpfMask from "../../functions/cpfMask";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 
 const initialForm = {
-  name:'', email:'', cpf:'', password:''
+  name:'', email:'', cpf:'', password:'', rPassword:''
 }
 
 export default function SignUp(){
   const [showPass, setShowPass] = useState({pass:false, rPass: false})
   const [alert, setAlert] = useState(<></>);
   const [form, setForm] = useForm(initialForm)
-  const [error, setError] = useState(false)
+  const [error, setError] = useState({email:false, password:false})
 
   const onChangeCpf = (e)=>{
     const newForm = {...form, cpf: cpfMask(e.target.value)}
@@ -25,7 +27,7 @@ export default function SignUp(){
     <All>
       <MyAllContent>
         <Logo>
-          <img src={logoRappi} />
+          <img src={logoRappi} alt={'logo rappi'}/>
         </Logo>
         <Text>Entrar</Text>
       <MyForm>
@@ -50,7 +52,7 @@ export default function SignUp(){
             onChange={setForm}
             type={'email'}
             placeholder={'email@email.com'}
-            error={error}
+            error={error.email}
             required
           />
           <MyInput
@@ -64,6 +66,52 @@ export default function SignUp(){
             placeholder={'000.000.000-00'}
             required
           />
+          <DivInput>
+            <MyInput
+              key={'password'}
+              variant={'outlined'}
+              name={'password'}
+              label={'Senha'}
+              value={form.password}
+              onChange={setForm}
+              type={showPass.pass? 'text' : 'password'}
+              placeholder={'Mínimo 6 caracteres'}
+              minLength={'6'}
+              error={error.password}
+              required
+            />
+            {/*<Senha src={logoSenha} onClick={()=>setShowPass(!showPass)}/>*/}
+            <MyIconButton>
+              {showPass.pass? (
+                <VisibilityIcon onClick={()=>setShowPass({pass: !showPass.pass, rPass: showPass.rPass})}/>
+              ):(
+                <VisibilityOffIcon onClick={()=>setShowPass({pass: !showPass.pass, rPass: showPass.rPass})}/>
+              )}
+            </MyIconButton>
+          </DivInput>
+          <DivInput>
+            <MyInput
+              key={'rPassword'}
+              variant={'outlined'}
+              name={'rPassword'}
+              label={'Confirmar senha'}
+              value={form.rPassword}
+              onChange={setForm}
+              type={showPass.rPass? 'text' : 'password'}
+              placeholder={'Mínimo 6 caracteres'}
+              minLength={'6'}
+              error={error.password}
+              required
+            />
+            {/*<Senha src={logoSenha} onClick={()=>setShowPass(!showPass)}/>*/}
+            <MyIconButton>
+              {showPass.rPass? (
+                <VisibilityIcon onClick={()=>setShowPass({pass: showPass.pass, rPass: !showPass.rPass})}/>
+              ):(
+                <VisibilityOffIcon onClick={()=>setShowPass({pass: showPass.pass, rPass: !showPass.rPass})}/>
+              )}
+            </MyIconButton>
+          </DivInput>
         </StylesProvider>
       </MyForm>
       </MyAllContent>
