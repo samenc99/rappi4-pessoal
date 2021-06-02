9addeaa16a974b3d4ea234995b87dc6b946eacd7
samenc99 <mateus.enc@gmail.com>
2021-06-02 08:38:56 -0300

    tratando os inputs

--- src/pages/Sign/SignUp.jsx
+++ src/pages/Sign/SignUp.jsx
@@ -1,5 +1,5 @@
 import All from "../../styleAll/styledAll";
-import {DivInput, Logo, MyAllContent, MyForm, MyIconButton, MyInput, Text} from "./styled";
+import {Button, DivInput, Logo, MyAlert, MyAllContent, MyForm, MyIconButton, MyInput, Text} from "./styled";
 import logoRappi from '../../assets/logo-rappi.svg'
 import {useEffect, useRef, useState} from "react";
 import useForm from "../../hooks/useForm";
@@ -7,6 +7,8 @@ import {StylesProvider} from "@material-ui/core/styles";
 import cpfMask from "../../functions/cpfMask";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import {validateEmail} from "./validateEmail";
+import {AlertTitle} from "@material-ui/lab";
 
 const initialForm = {
   name:'', email:'', cpf:'', password:'', rPassword:''
@@ -16,12 +18,48 @@ export default function SignUp(){
   const [showPass, setShowPass] = useState({pass:false, rPass: false})
   const [alert, setAlert] = useState(<></>);
   const [form, setForm] = useForm(initialForm)
-  const [error, setError] = useState({email:false, password:false})
+  const [error, setError] = useState({email:false, password:false, rPassword: false})
 
+  //interação com usuário
   const onChangeCpf = (e)=>{
     const newForm = {...form, cpf: cpfMask(e.target.value)}
     setForm(newForm)
   }
+  const onClick = (e)=>{
+    e.preventDefault()
+    if(!validateEmail(form.email)){
+      setError({email: true,password: false, rPassword: false})
+      setAlert(
+        <MyAlert severity={'error'}>
+          <AlertTitle><strong>Erro</strong></AlertTitle>
+          E-mail inválido
+        </MyAlert>
+      )
+      return
+    }
+    if(form.password.length<6){
+      setError({email: false,password: true, rPassword: false})
+      setAlert(
+        <MyAlert severity={'error'}>
+          <AlertTitle><strong>Atenção</strong></AlertTitle>
+          Senha precisa ter ao menos 6 caracteres
+        </MyAlert>
+      )
+      return
+    }
+    if(form.password!==form.rPassword){
+      setError({email: false,password: false, rPassword: true})
+      setAlert(
+        <MyAlert severity={'error'}>
+          <AlertTitle><strong>Erro</strong></AlertTitle>
+          Senha de confirmação está diferente
+        </MyAlert>
+      )
+      return
+    }
+  }
+
+
 
   return(
     <All>
@@ -30,7 +68,8 @@ export default function SignUp(){
           <img src={logoRappi} alt={'logo rappi'}/>
         </Logo>
         <Text>Entrar</Text>
-      <MyForm>
+        {alert}
+      <MyForm onSubmit={onClick}>
         <StylesProvider injectFirst>
           <MyInput
             key={'name'}
@@ -94,13 +133,13 @@ export default function SignUp(){
               key={'rPassword'}
               variant={'outlined'}
               name={'rPassword'}
-              label={'Confirmar senha'}
+              label={'Confirmar'}
               value={form.rPassword}
               onChange={setForm}
               type={showPass.rPass? 'text' : 'password'}
-              placeholder={'Mínimo 6 caracteres'}
+              placeholder={'Confirme a senha anterior'}
               minLength={'6'}
-              error={error.password}
+              error={error.rPassword}
               required
             />
             {/*<Senha src={logoSenha} onClick={()=>setShowPass(!showPass)}/>*/}
@@ -113,6 +152,7 @@ export default function SignUp(){
             </MyIconButton>
           </DivInput>
         </StylesProvider>
+        <Button>Criar</Button>
       </MyForm>
       </MyAllContent>
     </All>
